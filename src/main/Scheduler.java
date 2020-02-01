a15c644e25d3c9148012227dcfa02854c43612a5
Victor <olaitanvictor99@gmail.com>
2020-01-31 22:45:25 -0500

    Merge branch 'master' of https://github.com/DeveloperRic/sysc-3303-project

--- src/main/Scheduler.java
+++ src/main/Scheduler.java
@@ -3,6 +3,13 @@
  *
  */
 package main;
+
+/**
+ * The scheduler interface that controls the put and get functions
+ * 
+ * @author Kevin
+ *
+ */
 public interface Scheduler {
 
 	public Object get();
