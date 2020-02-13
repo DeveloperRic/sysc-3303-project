543d8c64aae3614b376f38029f46c0ac632129b8
Victor <olaitanvictor99@gmail.com>
2020-02-13 16:29:13 -0500

    Update ElevatorTest.java

--- src/test/ElevatorTest.java
+++ src/test/ElevatorTest.java
@@ -9,21 +9,18 @@ import main.elevator.ElevatorState;
 import main.elevator.ElevatorSubsystem;
 
 public class ElevatorTest {
-	
+
 	static MainScheduler m = new MainScheduler();
 	static SchedulerElevator s = new SchedulerElevator(m);
-	
-	
+
 	@Test
 	public void test() {
 
 		ElevatorSubsystem elevator = new ElevatorSubsystem(s);
 		elevator.powerOn();
 
-
 		ElevatorState state = elevator.getState();
-		
-		
+
 		System.out.println("\nAssigning a task");
 		state.assignTask(new Task("14:05:15.0", "2", "1", "4"));
 		System.out.println("\nAssigning a task");
@@ -31,14 +28,14 @@ public class ElevatorTest {
 		System.out.println("\nAssigning a task");
 		state.assignTask(new Task("14:13:56.06", "1", "-1", "3"));
 
-		
 		// wait for elevator to return to sleep mode
-		while(state.isAwake()){
+		while (state.isAwake()) {
 			try {
 				Thread.sleep(3000);
-			} catch (InterruptedException e) {}
+			} catch (InterruptedException e) {
+			}
 		}
-		
+
 	}
 
 }
