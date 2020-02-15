a7ed13e3e1b92d0d10f61ee7fb16ee7d363c8773
Austin Do <43225674+austin-do@users.noreply.github.com>
2020-02-15 17:28:16 -0500

    Update Floor.java

--- src/main/Floor.java
+++ src/main/Floor.java
@@ -2,31 +2,30 @@ package main;
 
 public class Floor {
 		int floorNumber;
-		Lamp upRequestLight;
-		Lamp downRequestLight;
-
-		FloorState bothRequested;
-		FloorState upRequested;
-		FloorState downRequested;
-		FloorState noRequest;
-
-		FloorState state;
-
+		public Lamp upRequestLight;
+		public Lamp downRequestLight;
+		
+		public FloorState bothRequested;
+		public FloorState upRequested;
+		public FloorState downRequested;
+		public FloorState noRequest;
+		
+		public FloorState state;
+		
 		//FLOOR CONSTRUCTOR
-		Floor (int floorNumber) {
+		public Floor (int floorNumber) {
 			upRequestLight = new Lamp();
 			downRequestLight = new Lamp();
-
+			
 			this.floorNumber = floorNumber;
-
+			
 			upRequested = new upRequested(this);
 			downRequested = new downRequested(this);
 			bothRequested = new bothRequested(this);
 			noRequest = new noRequest(this);
-
+			
 			state = noRequest;
 		}
-
 		//FLOORSTATE
 		public interface FloorState {
 			void requestUp();
@@ -40,12 +39,12 @@ public class Floor {
 			public upRequested(Floor _floor) {
 				floor = _floor;
 			}
-
+			
 			@Override
 			public void requestUp() {
-				System.out.println("Up requested on floor " + floorNumber);
+				System.out.println("FLOOR SUBSYSTEM: Up requested on floor " + floorNumber);
 				floor.upRequestLight.turnOn();
-
+				
 				if (downRequestLight.lampOn) {
 					floor.setState(bothRequested);
 				}
@@ -55,14 +54,14 @@ public class Floor {
 			public void requestDown() {
 				floor.setState(downRequested);
 				floor.requestDown();
-
+				
 			}
 
 			@Override
 			public void requestServed(int direction) {
 				switch (direction) {
 				case 1:
-					downRequestLight.turnOff();
+					upRequestLight.turnOff();
 					floor.setState(noRequest);
 					break;
 				default: 
@@ -70,14 +69,14 @@ public class Floor {
 				}	
 			}
 		}
-
+		
 		//DOWN REQUESTED
 		public class downRequested implements FloorState {
 			Floor floor;
 			public downRequested(Floor _floor) {
 				floor = _floor;
 			}
-
+			
 			@Override
 			public void requestUp() {
 				// If in downState switch to upState
@@ -88,9 +87,9 @@ public class Floor {
 			@Override
 			public void requestDown() {
 				// If upState already requested turn on light and then switch to bothState
-				System.out.println("Down requested on floor " + floorNumber);
+				System.out.println("FLOOR SUBSYSTEM: Down requested on floor " + floorNumber);
 				downRequestLight.turnOn();
-
+				
 				if (upRequestLight.lampOn) {
 					floor.setState(bothRequested);
 				}
@@ -108,22 +107,22 @@ public class Floor {
 				}
 			}
 		}
-
+		
 		//BOTH REQUESTED
 		public class bothRequested implements FloorState {
 			Floor floor;
 			public bothRequested(Floor _floor) {
 				floor = _floor;
 			}
-
+			
 			@Override
 			public void requestUp() {
-				System.out.println("Up requested on floor " + floorNumber);
+				System.out.println("FLOOR SUBSYSTEM: Up requested on floor " + floorNumber);
 			}
 
 			@Override
 			public void requestDown() {
-				System.out.println("Down requested on floor " + floorNumber);
+				System.out.println("FLOOR SUBSYSTEM: Down requested on floor " + floorNumber);
 			}
 
 			@Override
@@ -131,23 +130,27 @@ public class Floor {
 				switch (direction) {
 				case -1:
 					downRequestLight.turnOff();
-					floor.setState(upRequested);
+					if(upRequestLight.lampOn) {
+						floor.setState(upRequested);
+					} else { floor.setState(noRequest);}
 					break;
 				case 1: 
 					upRequestLight.turnOff();
-					floor.setState(downRequested);
+					if(downRequestLight.lampOn) {
+						floor.setState(downRequested);
+					} else { floor.setState(noRequest);}
 					break;
 				}
 			}
 		}
-
+		
 		//NO REQUESTS
 		public class noRequest implements FloorState {
 			Floor floor;
 			public noRequest(Floor _floor) {
 				floor = _floor;
 			}
-
+			
 			@Override
 			public void requestUp() {
 				floor.setState(upRequested);
@@ -158,7 +161,7 @@ public class Floor {
 			public void requestDown() {
 				floor.setState(downRequested);
 				floor.requestDown();
-
+				
 			}
 
 			@Override
@@ -166,19 +169,19 @@ public class Floor {
 				System.out.println("There are no requests on this floor.");
 			}
 		}
-
+		
 		public void requestDown() {
 			state.requestDown();
 		}
-
+		
 		public void requestUp() {
 			state.requestUp();
 		}
-
+		
 		public void requestServed(int direction) {
 			state.requestServed(direction);
 		}
-
+		
 		public void setState (FloorState _state) {
 			state = _state;
 		}
